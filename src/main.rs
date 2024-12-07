0de24426f9893a26a652b669f56583cf57054a65
Samurai_Chia <jyjzone@163.com>
2024-12-07 15:21:42 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    for number in(1..4){
+        println!("number is :{number}");
+    }
+
+    println!("|||||||||||||||||||||||");
+
+    for number in(1..4).rev(){
+        println!("number is :{number}");
+    }
+
 }
